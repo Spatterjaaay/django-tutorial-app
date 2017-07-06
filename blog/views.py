8da35e4ba26b4049da012ff26f4cbd1bc568249d
Spatterjaaay <aja.pursova@gmail.com>
2017-07-06 14:44:01 -0700

    added template, url and view for post details

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 
 def post_list(request):
     # search for blog posts that were published before now and sort them
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    # if there is no object with this pk, it will return error
+    # Post.objects.get(pk=pk)
+    # we want it to return 404 if no pk
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
