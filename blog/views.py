5bcf195fffca1997ff40380af771421b4ebd8e51
Spatterjaaay <aja.pursova@gmail.com>
2017-07-06 15:21:11 -0700

    created a form

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
-from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
+
 
 def post_list(request):
     # search for blog posts that were published before now and sort them
@@ -14,3 +17,16 @@ def post_detail(request, pk):
     # we want it to return 404 if no pk
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
